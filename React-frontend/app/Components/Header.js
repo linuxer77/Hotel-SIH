c55e13ed80909acbaacd767e40aa13873b87d105
linuxer77 <harshitg187@gmail.com>
2024-09-10 20:15:33 +0530

    reverted changes

--- React-frontend/app/Components/Header.js
+++ React-frontend/app/Components/Header.js
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { useRouter } from "next/navigation";
@@ -59,7 +60,7 @@ const Header = () => {
             onSubmit={handleSubmit}
           >
             <input
-              className="bg-transparent w-[300px] sm:w-400px focus:outline-none"
+              className="bg-transparent w-[300px] sm:w-[400px] focus:outline-none"
               type="text"
               placeholder="Explore with AI"
               value={searchQuery}
