42800e1b8d55f4fa3a50858c2f7b71970c349924
linuxer77 <harshitg187@gmail.com>
2024-09-12 10:09:56 +0530

    added proxy to local host

--- React-frontend/app/Components/Header.js
+++ React-frontend/app/Components/Header.js
@@ -11,16 +11,13 @@ const Header = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch(
-        "https://linuxer3443.pythonanywhere.com/search",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ query: searchQuery }),
-        }
-      );
+      const response = await fetch("http://127.0.0.1:5000/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ query: searchQuery }),
+      });
 
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
