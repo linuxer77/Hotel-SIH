9a888d3462cac2c70f9344c0ce06265c3b5550cd
linuxer77 <harshitg187@gmail.com>
2024-09-12 10:16:42 +0530

    added proxy to local host

--- React-frontend/app/Components/Header.js
+++ React-frontend/app/Components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://127.0.0.1:5000/search", {
+      const response = await fetch("http://localhost:5000/search", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
